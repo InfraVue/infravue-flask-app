26f799fd3542c3a7d720453b538f7975908a650f
InfraVue <akashmishra.en@gmail.com>
2025-05-05 00:06:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, flash, request
-from flask_sqlalchemy import SQLAlchemy
+from extensions import db
+from models import User
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
